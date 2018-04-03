f78cbd47c67b80703b363f9853de9a8c5603cc05
adrian rodriguez <adferove@hotmail.com>
2018-04-02 22:26:12 -0500

    ser

--- rices/src/main/java/co/com/rices/Conexion.java
+++ rices/src/main/java/co/com/rices/Conexion.java
@@ -3,7 +3,6 @@ package co.com.rices;
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
